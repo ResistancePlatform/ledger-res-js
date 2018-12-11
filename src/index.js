2176846f66bba72919d2c250c5bee27d2b51dc44
Luke Wegryn <lukewegryn@gmail.com>
2018-12-11 07:27:34 -0500

    Changed Tabs to Spaces

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import LedgerRes from './ledger-res'
 
 (async () => {
 
-	const logger = winston.createLogger({
-	  level: 'debug',
-	  format: winston.format.json(),
-	  transports: [
-	  	new winston.transports.Console({ format: winston.format.simple() })
-	  ]
-	});
+  const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.Console({ format: winston.format.simple() })
+    ]
+  });
 
-	let rpcClient = new Client({
+  let rpcClient = new Client({
     network: 'testnet',
     host: '127.0.0.1',
     port: 18232,
@@ -24,27 +24,27 @@ import LedgerRes from './ledger-res'
     timeout: 10000
   })
 
-	let ledgerRes = new LedgerRes(rpcClient)
-	await ledgerRes.init() //must call this to initialize connection to ledger
+  let ledgerRes = new LedgerRes(rpcClient)
+  await ledgerRes.init() //must call this to initialize connection to ledger
 
-	if(ledgerRes.btcTransport){
-	  console.log(await ledgerRes.getPublicKey(0))
+  if(ledgerRes.btcTransport){
+    console.log(await ledgerRes.getPublicKey(0))
 
-	  //send some coins
-	 	//set coin sending parameters
-	 	let ledgerBipIndex = 0
-	  let sendToAddress = 'rp5wwANPBfBaEEJCnTFGL7Nfa3Fp2cj3fq3'
-	  let txFee = 0.0001
-	  let payAmount = 1
+    //send some coins
+    //set coin sending parameters
+    let ledgerBipIndex = 0
+    let sendToAddress = 'rp5wwANPBfBaEEJCnTFGL7Nfa3Fp2cj3fq3'
+    let txFee = 0.0001
+    let payAmount = 1
 
-	  //sign the transaction using the ledger private key
-	  let signedTransaction = await ledgerRes.sendCoins(sendToAddress, ledgerBipIndex, txFee, payAmount)
-	  console.log(signedTransaction)
+    //sign the transaction using the ledger private key
+    let signedTransaction = await ledgerRes.sendCoins(sendToAddress, ledgerBipIndex, txFee, payAmount)
+    console.log(signedTransaction)
 
-	  //broadcast the transaction to the Resistance network
-	  let sentTransaction = await ledgerRes.sendRawTransaction(signedTransaction)
-	} else {
-		console.log("5 Ledger not accessible. Please make sure you have plugged in your Ledger, typed in your pin, and opened the Resistance application.")
-	}
+    //broadcast the transaction to the Resistance network
+    let sentTransaction = await ledgerRes.sendRawTransaction(signedTransaction)
+  } else {
+    console.log("5 Ledger not accessible. Please make sure you have plugged in your Ledger, typed in your pin, and opened the Resistance application.")
+  }
 
 })();
